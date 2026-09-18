f1a0d40cb7f3ea7fee634707dd6cbb8052f9f69f
Shweta Dumbare <shwetadumbare24@example.com>
2026-09-18 14:52:13 +0530

    Initial commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://studentmangsys.netlify.app/"
+        "https://studentmangsys.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
